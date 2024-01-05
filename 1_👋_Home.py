dcfa6eecd109d792d070dc209bce28e5b0c3e7d4
Zaid Hani <99472607+ZaidHani@users.noreply.github.com>
2024-01-05 10:04:38 +0300

    Update 1_👋_Home.py

--- 1_👋_Home.py
+++ 1_👋_Home.py
@@ -12,8 +12,8 @@ with open('Resume.pdf', 'rb') as file:
 about_me_html = '''
     <h3>😃 About Me</h3>
     <p>
-    Hello, I’m Zaid and I am a AI and Data Science student. I am currently taking my Bachelor’s degree in Data Scince from Al-Balqaa Applied University.
-    I’m passionate about working with huge datasets and making an effective use out of them.\n
+    Hello, I’m Zaid and I am an AI and Data Science student. I am currently taking my Bachelor’s degree in Data Science from Al-Balqaa Applied University.
+    I’m passionate about working with huge datasets and making effective use of them.\n
     With a solid foundation in programming and statistics, I’m able to do data analysis tasks effectively to extract meaningful insights from data.\n
     As a data science enthusiast, I harness the power of machine learning algorithms and predictive modeling to explore the potential within datasets.
     My work is grounded in a commitment to ethical data practices, ensuring that the insights generated contribute positively to the broader landscape.
